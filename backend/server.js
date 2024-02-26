665b2715de17519120663dc4909fac14d9c178cb
dhivagarpriyan <dhivagardhivagar4151@gmail.com>
2024-02-26 18:33:22 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -10,6 +11,8 @@ import userRoute from "./Routes/UserRoute.js"
 import ConnectToMongodb from "./db/ConnectToMongodb.js";
 import bodyParser from "body-parser";
 import { app, server } from "./socket/socket.js";
+
+
 app.use(
     cors({
     
@@ -17,6 +20,9 @@ app.use(
       credentials: true,
     })
   );
+
+  const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -26,16 +32,16 @@ app.use("/api/auth",AuthRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoute);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 
-app.get("/test",(req,res)=>{
-    res.json("running successfully")
-})
 
 server.listen(PORT,()=>{
     ConnectToMongodb()
     console.log(`server running on ${PORT}`)
 })
-
-//cBkL1vVCabPe0iRo
